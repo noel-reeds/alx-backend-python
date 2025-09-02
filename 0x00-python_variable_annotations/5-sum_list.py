879e93447741a64be1af2108c116d83ad50cc963
altoids <noelreeds01@gmail.com>
2025-09-02 10:15:25 +0300

    returns sum of list of float values

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""complex types - list of floats"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """returns their sum as a float"""
+    return sum(i for i in input_list)
