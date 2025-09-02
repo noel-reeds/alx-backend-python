397ad261486240d8c3e6e70f61dc5c84b7c76959
altoids <noelreeds01@gmail.com>
2025-09-02 10:24:33 +0300

    fixes TypeError

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """complex types - list of floats"""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """returns their sum as a float"""
     return sum(i for i in input_list)
