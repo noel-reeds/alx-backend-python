b0149005fe0dc78129d50600db7c73a41aef20ab
altoids <noelreeds01@gmail.com>
2025-09-02 16:01:24 +0300

    fixes typerror

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """complex types - functions"""
-from collections.abc import Callable
+from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
