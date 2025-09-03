4db68d7a422bcf54973ee6b891901d907fe0dd93
altoids <noelreeds01@gmail.com>
2025-09-03 15:50:54 +0300

    returns a random delay in secs

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""The basics of async"""
+import asyncio
+import random
+from typing import Awaitable
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """waits for a random delay and returns it"""
+    await asyncio.sleep(rd := random.uniform(0, max_delay))
+    return rd
