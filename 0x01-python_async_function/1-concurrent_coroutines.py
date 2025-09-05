c43abc83c67d7fbd2657a89c1bdc2d67cbc36d21
altoids <noelreeds01@gmail.com>
2025-09-05 19:27:55 +0300

    sorts random delays in ascending order

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,9 +6,17 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """return the list of all thewait_random(max_delay)"""
-    delays = []
-    for x in range(n):
+    """return the list of all wait_random(max_delay)"""
+    delays: List[float] = []
+    for m in range(n):
         rd = await wait_random(max_delay)
-        delays.append(rd)
+        if len(delays) < 1:
+            delays.append(rd)
+        else:
+            for i in range(len(delays)):
+                if rd < delays[i] or rd == delays[i]:
+                    delays.insert(i, rd)
+                    break
+                else:
+                    i += 1
     return delays
