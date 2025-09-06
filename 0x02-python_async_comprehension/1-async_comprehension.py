7944a9bd2fa06e3de7070a6dcfd9972b5d04dde5
altoids <noelreeds01@gmail.com>
2025-09-07 01:43:20 +0300

    return 10 float numbers

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Async Comprehensions"""
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List:
+    """return 10 random numbers"""
+    return [random_number async for random_number in async_generator()]
