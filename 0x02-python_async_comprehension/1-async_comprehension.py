8c069419bafd9f1bda5ad6f1d1132b6174696aa4
altoids <noelreeds01@gmail.com>
2025-09-07 01:47:56 +0300

    annotates ret-value

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,6 +4,6 @@ from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List:
+async def async_comprehension() -> List[float]:
     """return 10 random numbers"""
     return [random_number async for random_number in async_generator()]
