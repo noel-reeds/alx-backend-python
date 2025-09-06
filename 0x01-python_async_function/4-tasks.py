3f37637ca65462c214bb30a432c7ec4d245c611b
altoids <noelreeds01@gmail.com>
2025-09-06 19:23:33 +0300

    returns asyncio tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
-"""asyncio tasks"""
+"""multiple coroutines with async"""
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """return the list of asyncio tasks"""
-    tasks = []
-    for x in range(n):
-        task = task_wait_random(max_delay)
-        tasks.append(task)
+    """return the list of all wait_random(max_delay)"""
+    tasks: List[float] = []
+    for m in range(n):
+        rd = await task_wait_random(max_delay)
+        if len(tasks) < 1:
+            tasks.append(rd)
+        else:
+            for d in tasks:
+                if rd < d or rd == d:
+                    tasks.insert(tasks.index(d), rd)
+                    break
+                elif rd > tasks[len(tasks) - 1]:
+                    tasks.append(rd)
+                    break
     return tasks
