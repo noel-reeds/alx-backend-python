15acfb2df04639ec58e8b34b7dba1c2304291036
altoids <noelreeds01@gmail.com>
2025-09-07 01:48:58 +0300

    annotates ret-value

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Async Generator"""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """yield random numbers"""
     for i in range(10):
         rd = random.uniform(0, 10)
