fc963355e79ce044abbf81bff330b0334c0238ba
altoids <noelreeds01@gmail.com>
2025-09-15 12:40:47 +0300

    passes key error

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,8 +16,8 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(access_nested_map(nested_map, path), expected_return)
 
     @parameterized.expand([
-        ({}, ('a',), KeyError)
+        ({}, ('a',))
     ])
-    def test_access_nested_map_exception(self, nested_map, path, error):
-        with self.assertRaises(error):
+    def test_access_nested_map_exception(self, nested_map, path):
+        with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
