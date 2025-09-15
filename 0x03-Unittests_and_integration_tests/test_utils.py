65760fccbe193da61ac7cd75a03fc221c7da7675
altoids <noelreeds01@gmail.com>
2025-09-15 12:13:25 +0300

    unit tests for access_nested_map

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Parameterize a unit test"""
+import unittest
+from nose.tools import assert_equal
+from parameterized import parameterized, parameterized_class
+access_nested_map = __import__('utils').access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """unit test for utils.access_nested_map"""
+    @parameterized.expand([
+        ({'a': 1}, ('a',), 1),
+        ({'a': {'b': 2}}, ('a',), {'b': 2}),
+        ({'a': {'b': 2}}, ('a', 'b'), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected_return):
+        assert_equal(access_nested_map(nested_map, path), expected_return)
