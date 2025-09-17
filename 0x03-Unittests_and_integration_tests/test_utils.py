13c9305e2ec448ec48d8f722c05fa475b23cfd04
altoids <noelreeds01@gmail.com>
2025-09-17 15:09:32 +0300

    adds func docs

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,6 +16,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({'a': {'b': 2}}, ('a', 'b'), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected_return):
+        "Tests for access_nested_map function"
         self.assertEqual(access_nested_map(nested_map, path), expected_return)
 
     @parameterized.expand([
