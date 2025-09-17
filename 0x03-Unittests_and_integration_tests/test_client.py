c923d075127334ba7a1db183ee920b6e214f1274
altoids <noelreeds01@gmail.com>
2025-09-18 00:29:22 +0300

    tests for a _public_repos_url property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import unittest
-from unittest.mock import patch, Mock, PropertyMock
+from unittest.mock import patch, Mock, PropertyMock as PM
 from client import GithubOrgClient
+import client as client_module
 from parameterized import parameterized
 
 
@@ -11,12 +12,24 @@ class TestGithubOrgClient(unittest.TestCase):
         ('google', {'payload': True}),
         ('abc', {'payload': False}),
     ])
-    def test_org(self, org_name, res):
+    def test_org(self, org_name, response):
         """Test cases"""
-        with patch('client.get_json', new_callable=PropertyMock) as mocked:
-            mocked.return_value = res
+        with patch('client.get_json', return_value=response) as mocked:
             client = GithubOrgClient(org_name)
             json = client.org
 
         mocked.assert_called_once_with(client.ORG_URL.format(org=org_name))
-        self.assertEqual(json, res)
+        self.assertEqual(json, response)
+
+    @parameterized.expand([
+        ('google',),
+        ('abc',),
+    ])
+    def test_public_repos_url(self, name):
+        """Tests"""
+        github_client = GithubOrgClient(name)
+        with patch('client.GithubOrgClient.org', new_callable=PM) as mocked:
+            mocked.return_value = {'repos_url': 'some public repos'}
+            _repos = github_client._public_repos_url
+
+        self.assertEqual(_repos, 'some public repos')
